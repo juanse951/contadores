a977c24ac4589284255ea9c48f54940ab60c98d1
juanse951 <juanse951@gmail.com>
2024-07-05 09:16:12 -0500

    actividad CONTADORES curso uno Java alura latam

---
+++ app.js
@@ -0,0 +1,36 @@
+/*Crea un contador que comience en 1 y vaya hasta 10 
+usando un bucle 'while'. Muestra cada número.*/
+
+let contador = 0;
+
+while(contador != 10){
+    contador++;
+    //se mostrará en la consola el conteo de 1 a 10
+    console.log(contador);
+}
+
+/*Crea un contador que comience en 10 y vaya hasta 0 
+usando un bucle 'while'. Muestra cada número.*/
+
+let contadorr = 11;
+
+while(contadorr != 0){
+    contadorr--;
+    //se mostrará en la consola el conteo de 10 a 0
+    console.log(contadorr);
+}
+    
+/*Crea un programa de cuenta progresiva. Pide un número 
+y cuenta desde 0 hasta ese número utilizando un bucle 
+'while' en la consola del navegador.*/
+
+let numero = 0;
+let progresiva = prompt ("Escribe un numero")
+
+while(numero != progresiva ){
+    numero++;
+    //se mostrará en la consola el conteo progresivo
+    console.log(numero)
+}
+
+
